76fceed50a0d804845ff71390b6f81ac30f8f519
Alvaro Dorado Adame <alvarodoradoadame@gmail.com>
2022-03-16 00:41:12 +0000

    Finished Section 2

---
+++ Scot.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Scot.Services.ProductAPI.Models;
+
+namespace Scot.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Basic constructor for entityframework dbcontext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Scot.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Scot.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
